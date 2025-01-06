87a3227d1ee7ebe919412c9b76215f2f7063e088
kievlz <kievluizfg@gmail.com>
2025-01-06 20:45:27 -0300

    refactor: changing flag more than zero to less than zero

--- modules/ALU/flags_calculator.vhd
+++ modules/ALU/flags_calculator.vhd
@@ -16,7 +16,7 @@ begin
 	flags(2) <= not s(15) and not s(14) and not s(13) and not s(12) and not s(11) and not s(10) and not s(9) and not s(8) and not s(7) and not s(6) and not s(5) and not s(4) and not s(3) and not s(2) and not s(1) and not s(0);
 	
 -- vvvvvvv more than zero 	
-	flags(1) <= not s(15) and (s(14) or s(13) or s(12) or s(11) or s(10) or s(9) or s(8) or s(7) or s(6) or s(5) or s(4) or s(3) or s(2) or s(1) or s(0));
+	flags(1) <= s(15);
 	
 -- vvvvvvv overflow
 	flags(0) <= (cout_15 xor cout_14); 
