24b713da807559f926d2a21a29d7e28db11e0cd6
Gabriel Neto <gabrielsebastiao321@gmail.com>
2025-01-19 11:41:43 -0300

    modificando o tamanho da entrada dos display de 7 segmentos no CHRIST.vhd

--- CHRIST/CHRIST.vhd
+++ CHRIST/CHRIST.vhd
@@ -2,8 +2,8 @@ entity CHRIST is
 	port(
 		clk : in bit; -- TODO: PERS
 		ext_out_leds	   : out BIT_VECTOR(15 downto 0);
-		ext_out_7seg_10    : out BIT_VECTOR(15 downto 0);
-		ext_out_7seg_32    : out BIT_VECTOR(15 downto 0);
+		ext_out_7seg_10    : out BIT_VECTOR(13 downto 0);
+		ext_out_7seg_32    : out BIT_VECTOR(13 downto 0);
 		ext_in_switches    : in BIT_VECTOR(15 downto 0);
 		ext_in_buttons     : in BIT_VECTOR(15 downto 0)
 	);
