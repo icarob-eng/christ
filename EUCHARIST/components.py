4c4c44ac07adbb3d2e19b6e10a2d4972ed6ec8bd
icarob-eng <ibsc013@gmail.com>
2025-02-16 22:56:32 -0300

    feat: implements most of the emulated components

---
+++ EUCHARIST/components.py
@@ -0,0 +1,183 @@
+import abc
+import time
+from typing import Any
+from bitstring import Bits
+
+import json
+import numpy as np
+
+DTYPE = np.int16
+
+
+def split_repr(word: str | DTYPE) -> tuple[str, str, str, str, str]:
+    """Returns word in format (OPCODE, A, B, C, D)"""
+    word = word if isinstance(word, str) else Bits(int=word, length=16).bin
+    return word[:6], word[6:9], word[9:12], word[12:15], word[15]
+
+def opcode2mnemonic(opcode: str) -> str:
+    """Converts opcode to mnemonic"""
+    with open('instructions.json') as f:
+        instructions = json.load(f)
+
+    for mnemonic, data in instructions.items():
+        if opcode == data['bin']:
+            return mnemonic.upper()
+
+
+class RTLComponent(abc.ABC):
+    @abc.abstractmethod
+    def on_clock(self, cycle: int) -> None:
+        """General method of emulating RTL clock rising edge"""
+
+
+class AbsRegister(RTLComponent, abc.ABC):
+    @abc.abstractmethod
+    def write(self, data) -> None:
+        """External method to write data to component."""
+
+    @abc.abstractmethod
+    def read(self):
+        """External method to read data to component."""
+
+
+def Register(default_value: Any):
+    """Generates a register for the type of the default value, extending AbsRegister."""
+    dtype = type(default_value)
+    class _Register(AbsRegister):
+        def __init__(self):
+            self._value: dtype = default_value
+            self._buffer: dtype = default_value
+
+        def write(self, value: dtype) -> None:
+            self._buffer = value
+
+        def read(self) -> dtype:
+            return self._value
+
+        def on_clock(self, _: int) -> None:
+            self._value = self._buffer
+
+    return _Register()
+
+
+class DataComponent(RTLComponent):
+    def __init__(self, size: int, initials: np.ndarray[DTYPE] | None = None):
+        # noinspection PyTypeChecker
+        self._data: np.ndarray[DTYPE] = np.zeros(size, dtype=DTYPE)
+        self._buffer_value = DTYPE(0)
+        self._buffer_key = 0
+
+        if initials is not None and 0 < len(initials) < size:
+            n_ini = len(initials)
+            self._data[0:n_ini] = initials
+            self._buffer_value = initials[self._buffer_key]
+
+    def __setitem__(self, key: int, value: DTYPE) -> None:
+        if key < 0 or key >= len(self._data):
+            raise IndexError('Index out of range')
+
+        self._buffer_value = value
+
+    def __getitem__(self, key: int) -> DTYPE: return self._data[key]
+
+    def on_clock(self, _: int) -> None:
+        self._data = self._buffer_value[self._buffer_key]
+
+
+class RegisterFile(DataComponent):
+    def __init__(self, size: int):
+        super().__init__(size, None)  # prevents initialization of RF
+
+    def __setitem__(self, key: int, value: DTYPE) -> None:
+        if key == 0:  # prevents overwrite of RF[0]
+            return
+        else:
+            super().__setitem__(key, value)
+
+
+class Peripherals(RegisterFile):
+    """
+    Implemented peripherals [Readable/Writable]:
+    - LED: 001 W
+    - SEGA: 010 W
+    - SEGB: 011 W
+    - SW: 100 R
+    - BTN: 101 R
+    """
+
+    def __setitem__(self, key: int, value: DTYPE) -> None:
+        if key in [4, 5]:  # prevents writing in read-only regs
+            return
+        else:
+            super().__setitem__(key, value)
+
+    def on_clock(self, cycle: int) -> None:
+        super().on_clock(cycle)
+        # TODO: proper output
+        print('cycle:', cycle)
+        print(f'LEDS: [{bin(int(self._data[1]))[2:]}]')
+        print(f'SEG_A: [{int(self._data[1])[2:]:04X}]')
+        print(f'SEG_B: [{int(self._data[2])[2:]:04X}]')
+
+class CHRIST:
+    def __init__(self, source: np.ndarray[DTYPE], frequency: float):
+        self.mem = DataComponent(2**16, initials=source)
+        self.cache = DataComponent(2**7)
+        self.rf = RegisterFile(2**3)
+        self.pers = Peripherals(2**3)
+
+        self.pc = Register(DTYPE(0))
+        self.ir = Register(DTYPE(0))
+        self.flagr = Register((False, False, False))
+        self.s = Register(False)
+        # self.e = Register(False)
+
+        self.clock_cycle = 0
+        self._on_clock = [
+            self.mem,
+            self.cache,
+            self.rf,
+            self.pers,
+            self.pc,
+            self.ir,
+            self.flagr,
+            self.s
+        ]
+        self.delay = 1/frequency  # delay in secs
+
+    @property
+    def fetch_en(self): return not self.s
+    @property
+    def execute_en(self): return self.s
+
+    def start(self) -> None:
+        while True:
+            if self.fetch_en:
+                self.ir.write(self.mem[self.pc.read()])
+            if self.execute_en:
+                self.control_unit(*split_repr(Bits(int=self.ir.read(), length=16).bin))
+
+            for comp in self._on_clock:
+                comp.on_clock(self.clock_cycle)
+            self.clock_cycle += 1
+            time.sleep(self.delay)
+
+    def control_unit(self, opcode: str, a: str, b: str, c: str, d: str) -> None:
+        mnemonic = opcode2mnemonic(opcode)
+
+        if opcode[0:2] != '00':  # if not control
+            self.pc.write(self.pc.read() + 1)  # pc increment
+
+        match mnemonic:
+            case 'JUMP':
+                ...
+            case _:
+                ...
+
+    def compute_flags(self, number: int) -> tuple[bool, bool, bool]:
+        neg = number < 0
+        zero = number == 0
+        overflow = number > Bits(bin='0111111111111111').int or number < Bits(bin='1000000000000000').int
+        result = (neg, zero, overflow)
+        self.flagr.write(result)
+        return result
--- EUCHARIST/eucharist.py
+++ EUCHARIST/eucharist.py
@@ -1,4 +1,12 @@
-"""
+import argparse
+
+from components import *
+
+__version__ = '0.0.1'
+
+parser = argparse.ArgumentParser(
+    prog=f'EUCHARIST v{__version__}.',
+    description="""
 Emulation
 Using
 CHrist
@@ -6,5 +14,60 @@ Architecture and
 Resources of
 Integrated python
 Syntax
-Technology: EUCHARIST the CHRIST emulator
+Technology: EUCHARIST, the CHRIST emulator
 """
+)
+parser.add_argument(
+    'source',
+    type=str,
+    help='source code binary file.',
+)
+
+# TODO: add update frequency
+
+parser.add_argument(
+    '-s', '--string',
+    action='store_true',
+    help='open source in text mode (not binary mode).',
+)
+
+
+def parse_args() -> list[DTYPE]:
+    args = parser.parse_args()
+
+    try:
+        if args.string:
+            with open(args.source) as f:
+                lines = f.read().splitlines()
+
+                invalid_lines = []
+                for i, line in enumerate(lines):
+                    if len(line) != 16:
+                        invalid_lines.append(i)
+
+                if len(invalid_lines) > 0:
+                    raise ValueError(f'Invalid lines in {args.source}: {invalid_lines}')
+
+                return [np.int16(Bits(bin=line).int) for line in lines]
+        else:
+            # TODO: test this (SHOULD work)
+            return list(np.fromfile(args.source, dtype=DTYPE))
+    except FileNotFoundError:
+        raise FileNotFoundError(f'Source file {args.source} not found.')
+
+
+def print_formatted_code(lines: list[DTYPE] | np.ndarray[DTYPE], as_mnemonics=False) -> None:
+    i_size = len(str(len(lines)))
+    print(f'{"i":<{i_size}}:', 'opcode', 'A  ', 'B  ', 'C  ', 'D')
+    if as_mnemonics:
+        for i, line in enumerate(lines):
+            srepr = split_repr(line)
+            print(f'{i}:', f'{opcode2mnemonic(srepr[0]):<6}', *srepr[1:])
+    else:
+        for i, line in enumerate(lines):
+            print(f'{i}:', *split_repr(line))
+
+
+if __name__ == '__main__':
+    code = parse_args()
+    print_formatted_code(code, True)
